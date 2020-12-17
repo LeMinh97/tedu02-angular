bff9c0307d2a4e39325af0aa3d73bc2e3a2f259d
LeMinh97 <minhcrazy97@gmail.com>
2020-12-17 16:06:16 +0700

    start project

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './main.component';
+import { HomeComponent } from './home/home.component';
+import { ProductCategoryComponent } from './product-category/product-category.component';
+import { ProductComponent } from './product/product.component';
+import { UserComponent } from './user/user.component';
+
+@NgModule({
+  declarations: [MainComponent, HomeComponent, ProductCategoryComponent, ProductComponent, UserComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainModule { }
