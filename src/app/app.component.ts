166291729e5fae4376304670ab44dbafaf03d192
LeMinh97 <minhcrazy97@gmail.com>
2020-12-18 18:09:17 +0700

    continue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { Component, AfterViewChecked, ElementRef } from '@angular/core';
 })
 export class AppComponent implements AfterViewChecked {
   constructor(private elementRef: ElementRef) {
-
   }
+
   ngAfterViewChecked() {
     var s = document.createElement("script");
     s.type = "text/javascript";
