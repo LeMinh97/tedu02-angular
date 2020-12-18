206815693700ab423c6950adc5d74d5a47eca395
LeMinh97 <minhcrazy97@gmail.com>
2020-12-18 14:55:28 +0700

    completed lession 13

--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -7,6 +7,8 @@ export const mainRoutes: Routes = [
         path: '', component: MainComponent, children: [
             //localhost:4200/main
             { path: '', redirectTo: 'home', pathMatch: 'full' },
+            //localhost:4200/main/home
+            { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
             //localhost:4200/main/user
             { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }
         ]
